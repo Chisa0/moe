22481f6655e83a9cd83f8516119d37cac305a718
Chisa0 <3473598859@qq.com>
2022-08-16 00:44:06 +0000

    submit web

---
+++ JS/title.js
@@ -0,0 +1,15 @@
+var title_change = {
+	title_1 : function()
+	{
+		document.title = '1';
+	},
+	title_2 : function()
+	{
+		document.title = '2';
+	},
+	title_3 : function()
+	{
+		document.title = '0';
+	}}
+
+/*document.getElementsByTagName("TITLE")[0].onmouseover = title_change.title_1();*/
